d6de893ca919adcfff61b501ff417b896c89e201
Liana Donders <BlueLavaLamp93@gmail.com>
2017-05-10 15:04:26 -0700

    Update game.js

--- game.js
+++ game.js
@@ -14,14 +14,14 @@ Aquaplane.Preloader.prototype = {
 
     preload: function () {
 
-        game.load.spritesheet('town', 'assets/town.png', game.width, game.height, 22);
+        //game.load.spritesheet('town', 'assets/town.png', game.width, game.height, 22);
         
         this.load.path = 'assets/';
 
         this.load.bitmapFont('fat-and-tiny');
         this.load.bitmapFont('interfont');
 
-        this.load.images([ 'logo', 'man', 'husky', 'zomkabob', 'hand', 'zombie' ]);
+        this.load.images([ 'logo', 'man', 'husky', 'zomkabob', 'hand', 'zombie', 'town' ]);
         this.load.spritesheet('crack', 'Crack.png', 16, 6);
 //        var town = this.load.image('town', 'Town.png');
 //        town.height = game.height
@@ -43,10 +43,10 @@ Aquaplane.MainMenu = function () {};
 Aquaplane.MainMenu.prototype = {
 
     create: function () {
-        this.town=gmae.add.sprite(0, 0, "town");
-        this.town.animations.add('scroll', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22], 12, true);
-        this.town.animations.play("scroll");
-        //this.add.image(0, 0, 'town');
+        //this.town=gmae.add.sprite(0, 0, "town");
+        //this.town.animations.add('scroll', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22], 12, true);
+        //this.town.animations.play("scroll");
+        this.add.image(0, 0, 'town');
 
         var logo = this.add.image( this.world.centerX, 20, 'logo');
         logo.anchor.x = 0.5;
