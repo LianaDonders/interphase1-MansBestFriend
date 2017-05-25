799326c738141d0bb9354e806d9285dfa2027e29
Liana Donders <BlueLavaLamp93@gmail.com>
2017-05-25 13:48:53 -0700

    Update game.js

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ Aquaplane.Preloader.prototype = {
         this.load.bitmapFont('interfont');
 
         this.load.images([ 'logo', 'zomkabob', 'hand', 'zombie',]);
-        this.load.spritesheet('town', 'town.png', 800, 600, 22);
+        this.load.spritesheet('town', 'town.png', 600, 600, 22);
        
         this.load.spritesheet('crack', 'crack.png', 16, 6);
         this.load.spritesheet('man', 'man.png', 92, 92, 8);
