be5d7901e4fea600a489d45ba43913c0943b347e
Liana Donders <BlueLavaLamp93@gmail.com>
2017-05-31 13:55:11 -0700

    Update game.js

--- game.js
+++ game.js
@@ -142,7 +142,7 @@ Aquaplane.Game.prototype = {
         this.layer = this.add.group();
 
         this.man = this.layer.create(0, 0, 'man');
-        var man = game.add.sprite(0, 0, 'man');
+        var man = game.add.sprite(10, 10, 'man');
         var walk = man.animations.add('walk');
         man.animations.play('walk', 12, true)
 
@@ -154,7 +154,7 @@ Aquaplane.Game.prototype = {
         this.man.body.collideWorldBounds = false;
 
         this.husky = this.layer.create(0, 0, 'husky');
-        var husky = game.add.sprite(0, 0, 'husky');
+        var husky = game.add.sprite(10, 10, 'husky');
         var run = husky.animations.add('run');
         husky.animations.play('run', 12, true);
 
