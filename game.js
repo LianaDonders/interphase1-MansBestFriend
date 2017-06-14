6b79311964f3cb1be901cd093d153ec833e8ac0c
Liana Donders <BlueLavaLamp93@gmail.com>
2017-06-14 12:33:15 -0700

    Create game.js

--- game.js
+++ game.js
@@ -258,11 +258,11 @@ Aquaplane.Game.prototype = {
 
         if (frame === 'zombie')
         {
-            item.body.setSize(32, 14, 0, 24);
+            item.body.setSize(32, 14, 0, 30);
         }
         else
         {
-            item.body.setSize(16, 8, 24, 16);
+            item.body.setSize(16, 8, 24, 0);
         }
  if (frame === 'zomkabob')
         {
